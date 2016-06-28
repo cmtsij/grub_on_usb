b115e1096495663a498e390f7df79c8adee035b4
cmtsij <cmtsij@gmail.com>
2016-06-28 12:57:32 +0800

    sysresccd: extract.sh support alernative kernel

--- sysresccd/extract.sh
+++ sysresccd/extract.sh
@@ -3,4 +3,4 @@
 iso=${1:-systemrescuecd.iso}
 
 rm -rf iso 
-7z x $iso -oiso isolinux/initram.igz isolinux/rescue32 isolinux/rescue64 bootdisk/*
+7z x $iso -oiso isolinux/initram.igz isolinux/{rescue32,rescue64,altker32,altker64} bootdisk/*
