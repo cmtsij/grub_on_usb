78c3236fe3ded16e3eb9ee4d7a6e90eaef34dbf9
cmtsij <cmtsij@gmail.com>
2016-06-28 12:57:33 +0800

    wee: install by sudo

--- wee/wee_install.sh
+++ wee/wee_install.sh
@@ -4,6 +4,8 @@ device=${1:-/dev/sda}
 read -n 1 -p "Device is \"$device?\" [y/n]: " prompt
 echo
 
+SUDO=$(which sudo)
+
 if [ "$prompt" = "y" ]; then
-	sudo sh -x wee.sh -s wee_script.txt -d $device -f -v -u
+	${SUDO} sh -x wee.sh -s wee_script.txt -d $device -f -v -u
 fi
